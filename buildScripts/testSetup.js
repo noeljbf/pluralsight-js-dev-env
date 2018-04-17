2d6ffd32b1b7089bcf4731766c97f6187012f5ad
JB NOEL <jeanbaptiste.noel.09@gmail.com>
2018-04-17 21:40:37 +0200

    màj dernières modif 17/04/18

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,6 @@
+
+//Register Babel to transpile before our tests run
+require('babel-register')();
+
+//Disable Webpack features that Mocha doesn't understand
+require.extensions['.css'] = function() {};
